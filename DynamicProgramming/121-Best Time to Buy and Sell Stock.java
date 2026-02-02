19ec2121fe89073d50e0071e4417aad23a7b8045
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-02 18:45:41 +0530

    Previously solved problem

---
+++ DynamicProgramming/121-Best Time to Buy and Sell Stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        int MaxProfit=0;
+        int min=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<prices[min]){
+                min=i;
+            }
+            else{
+                profit=prices[i]-prices[min];
+                MaxProfit=Math.max(profit,MaxProfit);
+            }
+        }
+        
+        return MaxProfit;
+    }
+}
