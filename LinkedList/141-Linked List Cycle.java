9d3c8c8f35c1ea9036052d1dabfa18c93bbe005c
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-03 18:07:12 +0530

    Previously solved problem

---
+++ LinkedList/141-Linked List Cycle.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode first=head;
+        ListNode second=head;
+        while(second != null && second.next != null){
+            first=first.next;
+            second=second.next.next;
+            if(first == second){
+                return true;
+            }
+        }
+        return false;
+    }
+}
