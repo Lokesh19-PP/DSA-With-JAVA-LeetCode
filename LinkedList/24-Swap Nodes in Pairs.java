c37523309f8b578e843a6a808c1c1100c18d5c1b
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-04 19:37:18 +0530

    Previously solved problem

---
+++ LinkedList/24-Swap Nodes in Pairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode temp = new ListNode(0);
+        temp.next=head;
+        ListNode prev=temp;
+        
+        while(prev.next != null && prev.next.next != null){
+            ListNode first=prev.next;
+            ListNode second=first.next;
+            prev.next=second;
+            first.next=second.next;
+            second.next=first;
+            prev=first;
+        }
+        return temp.next;
+    }
+}
