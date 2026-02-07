4406c7ef8eb966ae9ced22653da2924ac696e094
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-07 14:14:03 +0530

    07-02-26 : Easy level problem

---
+++ Strings/14-Longest Common Prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        String common = "";
+        for(int i=0;i<first.length();i++){
+            if(first.charAt(i) == last.charAt(i)){
+                common = common + first.charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+        return common;
+    }
+}
