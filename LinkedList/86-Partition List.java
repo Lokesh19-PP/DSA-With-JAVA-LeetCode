813ca234ad85b6f1b20cea71cc8ccffdbe46d54c
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-15 19:50:38 +0530

    Previously solved problem

---
+++ LinkedList/86-Partition List.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        ListNode lefttail = left;
+        ListNode righttail = right;
+        while(head != null){
+            if(head.val < x){
+                lefttail.next = head;
+                lefttail = lefttail.next;
+            }
+            else{
+                righttail.next = head;
+                righttail = righttail.next;
+            }
+            head = head.next;
+        }
+        lefttail.next = right.next;
+        righttail.next = null;
+        return left.next;
+    }
+}
