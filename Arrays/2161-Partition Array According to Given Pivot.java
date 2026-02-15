4f4fa359072120d10e6fc2e82326bd5bfa681e7e
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-15 19:44:20 +0530

    15-02-26 : medium level problem

---
+++ Arrays/2161-Partition Array According to Given Pivot.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int[] ans = new int[nums.length];
+        int left = 0;
+        int right = nums.length-1;
+        for(int i=0,j=nums.length-1;i<nums.length;i++,j--){
+            if(nums[i] < pivot){
+                ans[left] = nums[i];
+                left++;
+            }
+            if(nums[j] > pivot){
+                ans[right] = nums[j];
+                right--; 
+            }
+        }
+        while(left <= right){
+            ans[left] = pivot;
+            left++;
+        }
+        return ans;
+    }
+}
