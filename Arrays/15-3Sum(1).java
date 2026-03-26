8e2b93eb85ef549279f6b60f0795d980509c0835
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-03-26 21:20:26 +0530

    26-03-26 : medium level problem

---
+++ Arrays/15-3Sum(1).java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.length-1;
+            int sum = -1 * nums[i];
+            while(left<right){
+                int s = nums[left]+nums[right];
+                if(s==sum){
+                    list.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
+                }
+                else if(sum < s){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return list;
+    }
+}
