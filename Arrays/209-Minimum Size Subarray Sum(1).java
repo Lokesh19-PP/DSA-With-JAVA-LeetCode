5ed7dfb0aaf4ce9b01a6c2837d14dc4d9890b986
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-04-01 19:38:59 +0530

    01-04-26 : medium level problem(Optimal Solution)

---
+++ Arrays/209-Minimum Size Subarray Sum(1).java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int res = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int right=0;right<nums.length;right++){
+            sum = sum + nums[right];
+            while(sum >= target){
+                res = Math.min(res, right - left + 1);
+                sum = sum - nums[left];
+                left++;
+            }
+        }
+        return res == Integer.MAX_VALUE?0:res;
+    }
+}
