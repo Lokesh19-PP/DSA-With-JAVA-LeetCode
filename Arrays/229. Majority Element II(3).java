550aff51540f32514e685a0bf0160f26d6d78ac0
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-08-15 14:05:53 +0530

    15-08-26 : Medium Level Problem

---
+++ Arrays/229. Majority Element II(3).java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int count1 = 0, count2 = 0;
+        int el1 = 0, el2 = 0;
+        int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
+        for(int num : nums){
+            if(el1 == num){
+                count1++;
+            }
+            else if(el2 == num){
+                count2++;
+            }
+            else if(count1 == 0){
+                count1 = 1;
+                el1 = num;
+            }
+            else if(count2 == 0){
+                count2 = 1;
+                el2 = num;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for(int num : nums){
+            if(num == el1) count1++;
+            else if(num == el2) count2++;
+        }
+        if(count1 > n/3) ans.add(el1);
+        if(count2 > n/3) ans.add(el2);
+        return ans;
+    }
+}
