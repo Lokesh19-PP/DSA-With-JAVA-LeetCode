12a07ebfbe7fde05e90ed9f989e5c503e9fdaf28
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-09-08 20:25:38 +0530

    08-09-26 : Striver's Sheet Problem

---
+++ Arrays/Find out how many times the array is rotated.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKRotation(ArrayList<Integer> nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            if(nums.get(mid) > nums.get(high)) low = mid + 1;
+            else high = mid;
+        } 
+        return low;
+    }
+}
