50808cf279fc17551fa93f00addf321b0b96752a
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-09-22 22:37:57 +0530

    21-09-26 : Medium Level Problem(Optimal)

---
+++ Arrays/275-H-Index II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int low = 0;
+        int high = citations.length-1;
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(citations[mid] >= citations.length-mid){
+                ans = citations.length - mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
